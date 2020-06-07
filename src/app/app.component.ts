06b219eb053e4d6c286418e2ad250ca9b76bdb3c
grinvald89 <v.greenwald@watcomdc.ru>
2020-06-07 07:46:48 +0300

    Добавлено блокирование загрузки модулей до получения конфигурации приложения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { AppConfigService } from './app-config/app-config.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'study-english-ui';
+export class AppComponent implements OnDestroy {
+    private appConfigLoaded: boolean = false;
+    private destructor$: Subject<boolean> = new Subject<boolean>();
+
+    get AppConfigLoaded(): boolean {
+        return this.appConfigLoaded;
+    }
+    set AppConfigLoaded(value: boolean) {
+        this.appConfigLoaded = value;
+        this.changeDetector.detectChanges();
+    }
+
+    constructor(
+        private readonly appConfigService: AppConfigService,
+        private readonly changeDetector: ChangeDetectorRef
+    ) {
+        this.appConfigService.init()
+            .pipe(takeUntil(this.destructor$))
+            .subscribe(_ => this.appConfigLoaded = true);
+    }
+
+    /**
+     * Деструктор компонента
+     */
+    public ngOnDestroy(): void {
+        this.destructor$.next(true);
+        this.destructor$.complete();
+    }
 }
