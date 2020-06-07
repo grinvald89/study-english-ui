d9757abe971e9fde9c77730232c49f9a9602b7e3
grinvald89 <v.greenwald@watcomdc.ru>
2020-06-07 08:59:22 +0300

    Добавлена маршрутизация для главной страницы

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { AddWordPageModule } from './pages/add-word-page/bootstrap/add-word-page.module';
+import { WordsStudyPageModule } from './pages/words-study-page/bootstrap/words-study-page.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/words-study',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        component: AppComponent,
+        children: [
+            {
+                path: 'words-study',
+                loadChildren: './pages/words-study-page/bootstrap/words-study-page.module#WordsStudyPageModule'
+            },
+            {
+                path: 'add-word',
+                loadChildren: './pages/add-word-page/bootstrap/add-word-page.module#AddWordPageModule'
+            }
+        ]
+    }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        WordsStudyPageModule,
+        AddWordPageModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule, Type, ModuleWithProviders } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DailyStatisticsModule } from '../statistics/daily-statistics/bootstrap/daily-statistics.module';
+import { WordsStudyPageModule } from './pages/words-study-page/bootstrap/words-study-page.module';
+
+/**
+ * Модули статистики
+ */
+const STATISTICS_MODULES: Type<any>[] | ModuleWithProviders[] | any[] = [
+    DailyStatisticsModule
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        STATISTICS_MODULES,
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
